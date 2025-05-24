91f9f6b9c58014a9b05d3f2593dea1148670426e
Rafael Camargo <rafaelhenrique1320@gmail.com>
2025-05-24 12:15:38 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,37 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 module "network" {
-  source = "./modules/network"
+  source              = "./modules/network"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  vnet_name           = var.vnet_name
+  subnet_name         = var.subnet_name
 }
 
 module "aks" {
-  source = "./modules/aks"
+  source              = "./modules/aks"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  subnet_id           = module.network.subnet_id
+  aks_name            = var.aks_name
 }
 
 module "functions" {
-  source = "./modules/functions"
+  source              = "./modules/functions"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  storage_account     = var.storage_account_name
+}
+
+module "keyvault" {
+  source              = "./modules/keyvault"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  keyvault_name       = var.keyvault_name
 }
